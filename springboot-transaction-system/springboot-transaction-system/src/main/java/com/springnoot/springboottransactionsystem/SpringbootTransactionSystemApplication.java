8bfd4efa69464873ae5cf208ecc21751f607eac1
Mudassir <mudassarmr988@gmail.com>
2023-01-01 21:03:36 +0530

    commit 1

--- springboot-transaction-system/springboot-transaction-system/src/main/java/com/springnoot/springboottransactionsystem/SpringbootTransactionSystemApplication.java
+++ springboot-transaction-system/springboot-transaction-system/src/main/java/com/springnoot/springboottransactionsystem/SpringbootTransactionSystemApplication.java
@@ -2,16 +2,16 @@ package com.springnoot.springboottransactionsystem;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SpringBootApplication
-public class SpringbootTransactionSystemApplication {
+@EnableTransactionManagement
+public class SpringbootTransactionDemoApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SpringbootTransactionSystemApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootTransactionDemoApplication.class, args);
+    }
 
 }
 
-
 // this is a springboot applicationn that is being downloaded from "start.spring.io" website
-
--- springboot-transaction-system/springboot-transaction-system/src/test/java/com/springnoot/springboottransactionsystem/SpringbootTransactionSystemApplicationTests.java
+++ springboot-transaction-system/springboot-transaction-system/src/test/java/com/springnoot/springboottransactionsystem/SpringbootTransactionSystemApplicationTests.java
@@ -9,5 +9,4 @@ class SpringbootTransactionSystemApplicationTests {
 	@Test
 	void contextLoads() {
 	}
-
 }
